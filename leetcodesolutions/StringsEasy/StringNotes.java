a154333939ae8715dfbd21e2943f5eaa73869d82
Archana <archana12n@gmail.com>
2018-07-12 23:13:59 -0700

    Leetcode

---
+++ leetcodesolutions/StringsEasy/StringNotes.java
@@ -0,0 +1,22 @@
+package com.basics.algorithm.archana.leetcodesolutions.StringsEasy;
+
+/**
+ Replace a character at a specific index in a string?
+ * <p>
+ * String are immutable in Java. You can't change them.
+ * <p>
+ * You need to create a new string with the character replaced.
+ * <p>
+ * String myName = "domanokz";
+ * String newName = myName.substring(0,4)+'x'+myName.substring(5);
+ * Or you can use a StringBuilder:
+ * <p>
+ * StringBuilder myName = new StringBuilder("domanokz");
+ * myName.setCharAt(4, 'x');
+ * <p>
+ * System.out.println(myName);
+ */
+
+public class StringNotes {
+
+}
